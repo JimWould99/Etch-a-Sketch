f7a844a4a7bd3b18aa7f646a83f8cfaf94c3054f
JimWould99 <130195351+JimWould99@users.noreply.github.com>
2024-01-03 14:47:57 +0000

    final

--- file.js
+++ file.js
@@ -9,21 +9,27 @@ for (i = 0; i < squareNumber * squareNumber; i++) {
     div.classList.add('children')
     div.setAttribute('style', `width: ${(400 - (squareNumber * 1.4))/squareNumber}px `)
     container.appendChild(div)
+}
     children = document.querySelectorAll('.children')
+    find()
 }
+  
+function find() {
+
+    children.forEach((button) => {
+        button.addEventListener('mouseover', () => {
+            button.style.backgroundColor = 'blue';
+        })
+    });
+    
+    children.forEach((button) => {
+        button.addEventListener('mouseout', () => {
+            button.style.backgroundColor = '';
+        })
+    });
+    
 }
 
-children.forEach((button) => {
-    button.addEventListener('mouseover', () => {
-        button.style.backgroundColor = 'blue';
-    })
-});
-
-children.forEach((button) => {
-    button.addEventListener('mouseout', () => {
-        button.style.backgroundColor = '';
-    })
-});
 
 function changeSquares() {
     squareNumber = prompt('type in number of squares');
