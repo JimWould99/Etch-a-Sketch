565e7a793c17379cf6215d6450ece2116e21bf81
JimWould99 <130195351+JimWould99@users.noreply.github.com>
2024-01-03 14:29:38 +0000

    attempt

---
+++ file.js
@@ -0,0 +1,36 @@
+container = document.querySelector('#container')
+change = document.querySelector('button')
+
+addSquares(10)
+
+function addSquares(squareNumber) {
+for (i = 0; i < squareNumber * squareNumber; i++) {
+    var div = document.createElement('div')
+    div.classList.add('children')
+    div.setAttribute('style', `width: ${(400 - (squareNumber * 1.4))/squareNumber}px `)
+    container.appendChild(div)
+    children = document.querySelectorAll('.children')
+}
+}
+
+children.forEach((button) => {
+    button.addEventListener('mouseover', () => {
+        button.style.backgroundColor = 'blue';
+    })
+});
+
+children.forEach((button) => {
+    button.addEventListener('mouseout', () => {
+        button.style.backgroundColor = '';
+    })
+});
+
+function changeSquares() {
+    squareNumber = prompt('type in number of squares');
+    container.innerHTML = ''
+    addSquares(squareNumber)
+}
+change.addEventListener('click', () => {
+    changeSquares()
+
+});
